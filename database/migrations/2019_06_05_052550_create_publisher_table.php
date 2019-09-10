95ad298ecfeaccec71c88b00abb69fdbf83b82c3
changaiqing <changaiqing@vip.163.com>
2019-09-10 20:10:26 +0800

    开发完成 bug 修改

--- database/migrations/2019_06_05_052550_create_publisher_table.php
+++ database/migrations/2019_06_05_052550_create_publisher_table.php
@@ -16,6 +16,7 @@ class CreatePublisherTable extends Migration
         Schema::create(config('book.database.table_prefix') . 'publisher', function (Blueprint $table) {
             $table->increments('id')->comment('唯一标识');
             $table->string('name')->unique()->comment('名称');
+            $table->string('cover')->nullable()->comment('logo');
             $table->string('desc')->nullable()->comment('备注');
 
             $table->timestamp('created_at')->nullable()->comment('创建时间');
